a715dff279124ebf4e296b094bd64748595332c7
boohyunsik <bhs9194@nate.com>
2023-11-25 00:06:57 +0900

    fix: resize og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
     <meta property="og:title" content="Riverbank Exchange" />
     <meta property="og:description" content="Check KRW exchange rate easy!" />
-    <meta property="og:image" content="https://riverbank-exchange.s3.ap-northeast-2.amazonaws.com/riverbank_logo_color.png" />
+    <meta property="og:image" content="https://riverbank-exchange.s3.ap-northeast-2.amazonaws.com/riverbank_logo_color_og.png" />
       <body className={inter.className}>{children}</body>
     </html>
   )
