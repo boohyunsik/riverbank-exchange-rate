d21056dd825f80b0b9f873f24cc3cabf048d604c
boohyunsik <bhs9194@nate.com>
2024-01-18 01:54:49 +0900

    fix: favicon for google seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         <meta property="og:image"
               content="https://riverbank-exchange.s3.ap-northeast-2.amazonaws.com/riverbank_logo_color_og.png"/>
         {/*<link rel="canonical" href="https://www.riverbank.world"/>*/}
-        <link rel="icon" href="favicon.ico"/>
+        <link rel="icon" href="/app/favicon.ico"/>
         <script async
                 src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCouOdyOcxG7pH4uuohbjIN6_q5zpaHwlk&callback=console.debug&libraries=maps,marker&v=beta">
         </script>
