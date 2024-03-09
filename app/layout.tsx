f0638ed3bd870febce4a01e88ec6568ca33d8d76
boosik-riverbank <boosik@riverbank.world>
2024-03-09 12:15:49 +0900

    feat: add facebook meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
         <meta property="og:image"
               content="https://riverbank-exchange.s3.ap-northeast-2.amazonaws.com/riverbank_logo_color_og.png"/>
         {/*<link rel="canonical" href="https://www.riverbank.world"/>*/}
+        <meta name="facebook-domain-verification" content="fnr755kdfj9dtaokhqsz5xeel2kb3l" />
         <link rel="icon" href="/app/favicon.ico"/>
         <script async
                 src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCouOdyOcxG7pH4uuohbjIN6_q5zpaHwlk&callback=console.debug&libraries=maps,marker&v=beta">
